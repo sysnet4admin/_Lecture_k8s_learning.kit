f561cff067075d0d699f3ead13864fe9399902b7
sysnet4admin <pagaia@hotmail.com>
2021-08-09 21:05:40 +0900

    sync-up

--- ch9/9.5/metallb-unintaller.sh
+++ ch9/9.5/metallb-unintaller.sh
@@ -2,9 +2,9 @@
 
 raw_git="raw.githubusercontent.com/sysnet4admin/IaC/master/manifests" 
 
-kubectl delete -f https://$raw_git/svc/metallb-0.9.6.yaml
-kubectl delete -f https://$raw_git/svc/metallb-l2config.yaml
 kubectl delete secret memberlist -n metallb-system
+kubectl delete -f https://$raw_git/svc/metallb-l2config.yaml
+kubectl delete -f https://$raw_git/svc/metallb-0.9.6.yaml
 
 echo "MetalLB uninstalled successfully"
 
