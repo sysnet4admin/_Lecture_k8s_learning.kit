f03225147025c17643e03af72460af7cc7b9b8f4
sysnet4admin <pagaia@hotmail.com>
2021-08-11 15:13:15 +0900

    rmv space

--- ch6/6.4/node_labels.sh
+++ ch6/6.4/node_labels.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 kubectl label node w1-k8s gpupool=nvidia accelerator=tesla-a100
 kubectl label node w2-k8s gpupool=nvidia accelerator=tesla-v100
 kubectl label node w3-k8s inmemory=redis  
