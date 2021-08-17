03d61b2d3535e6e301afe5a16553def213003547
sysnet4admin <pagaia@hotmail.com>
2021-08-17 11:18:12 +0900

    upt labels

--- ch6/6.4/node_labels.sh
+++ ch6/6.4/node_labels.sh
@@ -2,4 +2,4 @@
 
 kubectl label node w1-k8s gpupool=nvidia accelerator=tesla-a100
 kubectl label node w2-k8s gpupool=nvidia accelerator=tesla-v100
-kubectl label node w3-k8s inmemory=redis  
+kubectl label node w3-k8s diskint=nvme inmemory=redis
