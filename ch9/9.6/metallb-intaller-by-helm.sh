011223590bd0a6f2327db732a4f04701ba9065c7
sysnet4admin <pagaia@hotmail.com>
2021-08-21 13:34:05 +0900

    upt code

--- ch9/9.6/metallb-intaller-by-helm.sh
+++ ch9/9.6/metallb-intaller-by-helm.sh
@@ -5,6 +5,8 @@
 helm install metallb edu/metallb \
      --create-namespace \
      --namespace=metallb-system \
+     --set controller.image.tag=v0.10.2 \
+     --set speaker.image.tag=v0.10.2 \
      -f ~/_Lecture_k8s_learning.kit/ch9/9.6/l2-config-by-helm.yaml
 
 
