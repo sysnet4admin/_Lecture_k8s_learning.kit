6ff52f35b7a0749e2171f6e9a7e6e3f7a9357fca
sysnet4admin <pagaia@hotmail.com>
2021-08-21 14:12:42 +0900

    upt dir

--- ch9/9.6/installer-by-helm/metallb-intaller-by-helm.sh
+++ ch9/9.6/installer-by-helm/metallb-intaller-by-helm.sh
@@ -7,6 +7,6 @@ helm install metallb edu/metallb \
      --namespace=metallb-system \
      --set controller.image.tag=v0.10.2 \
      --set speaker.image.tag=v0.10.2 \
-     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/l2-config-by-helm.yaml
+     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/l2-config-by-helm.yaml
 
 
--- ch9/9.6/installer-by-helm/nfs-provisioner-installer-by-helm.sh
+++ ch9/9.6/installer-by-helm/nfs-provisioner-installer-by-helm.sh
@@ -3,8 +3,7 @@
 # path already provided by ch5 
 # default storageClass.name is nfs-client 
 
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+helm install nfs-provisioner edu/nfs-subdir-external-provisioner \
     --set nfs.server=192.168.1.10 \
     --set nfs.path=/nfs_shared/dynamic-vol \
     --set storageClass.name=managed-nfs-storage
-
