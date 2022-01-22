bb5143e13d83aa1bdbf4ee18f44b685bdcb827f0
sysnet4admin <pagaia@hotmail.com>
2022-01-22 18:11:48 +0900

    upt cp-istioctl

--- A/A.011/01.istio/cp-istioctl.sh
+++ A/A.011/01.istio/cp-istioctl.sh
@@ -3,4 +3,8 @@
 # install istioctl  
 curl -L  https://github.com/sysnet4admin/BB/raw/main/istioctl/v1.12.2/istioctl -o /usr/local/bin/istioctl
 chmod 744 /usr/local/bin/istioctl
+
+curl -L  https://github.com/sysnet4admin/BB/raw/main/istioctl/v1.12.2/istioctl.bash -o /etc/bash_completion.d/istioctl.bash
+echo 'source /etc/bash_completion.d/istioctl.bash' >> ~/.bashrc
+
 echo "Successfully installed istioctl"
