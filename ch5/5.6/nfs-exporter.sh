8a8c4b4ce2a4e9f26a8bc8c5353ee91e2b170002
Hoon Jo <pagaia@hotmail.com>
2022-04-12 19:49:05 +0900

    auto select running platform

--- ch5/5.6/nfs-exporter.sh
+++ ch5/5.6/nfs-exporter.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 nfsdir=/nfs_shared/$1
+if [[ -f /etc/redhat-release ]]; then 
+  svcname=nfs 
+elif [[ -f /etc/lsb-release ]]; then 
+  svcname=nfs-server 
+fi 
 
 if [ $# -eq 0 ]; then
   echo "usage: nfs-exporter.sh <name>"; exit 0
@@ -12,8 +17,9 @@ fi
 if [[ ! -d $nfsdir ]]; then
   mkdir -p $nfsdir
   echo "$nfsdir 192.168.1.0/24(rw,sync,no_root_squash)" >> /etc/exports
-  if [[ $(systemctl is-enabled nfs) -eq "disabled" ]]; then
-    systemctl enable nfs
+  if [[ $(systemctl is-enabled $svcname) -eq "disabled" ]]; then
+    systemctl enable $svcname
   fi
-    systemctl restart nfs
+    systemctl restart $svcname 
 fi
+
