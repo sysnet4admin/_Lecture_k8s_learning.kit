5eb3763ad77a535b0e1c252e3fea8b50c739e167
sysnet4admin <pagaia@hotmail.com>
2022-04-24 19:59:35 +0900

    upt pod-cnt110

--- A/A.019/.cmd
+++ A/A.019/.cmd
@@ -1,1 +1,12 @@
-[root@m-k8s ~]# k get po -A -o wide --field-selector spec.nodeName=w3-k8s
+watch kubectl top node 
+watch kubectl get deploy 
+
+k apply -f <pod-cnt110.yaml>
+k get po -o wide 
+k scale deploy pod-cnt110 --replicas=10
+.... (if you have fully enough computer, directly to the 110 pods)
+k scale deploy pod-cnt110 --replicas=20
+....30 40 50 60 70 80 90 100 
+k scale deploy pod-cnt110 --replicas=110
+watch kubectl get deploy
+k get po -A -o wide --field-selector spec.nodeName=w3-k8s | grep Running | wc -l 
