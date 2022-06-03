c6a73873e24eefe01405491f75de5e3731e769b3
Hoon Jo <pagaia@hotmail.com>
2022-06-03 14:21:52 +0900

    upt cmd

--- A/A.022/.cmd
+++ A/A.022/.cmd
@@ -1,5 +1,9 @@
 kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/" | jq
 
+kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/" | jq | grep pods
+kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/namespaces/default/pods/*/memory_max_usage_bytes" | jq
+
+k delete po -n custom-metrics -l app=custom-metrics-apiserver
 kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/namespaces/default/pods/*/nginx_http_requests_per_second" | jq
 
 
