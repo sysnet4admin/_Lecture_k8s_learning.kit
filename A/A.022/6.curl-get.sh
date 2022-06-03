b3cdbe6f1c887bbbc7da2a1e488022054b3e941e
Hoon Jo <pagaia@hotmail.com>
2022-06-03 15:42:45 +0900

    rmv sleep

--- A/A.022/6.curl-get.sh
+++ A/A.022/6.curl-get.sh
@@ -5,5 +5,5 @@ while true
 do
   COUNTER=$((COUNTER + 1))
   echo -ne "$COUNTER - " ; curl -s $1 | grep title
-  sleep 0.$[ ( $RANDOM % 9 ) + 1 ]s # sleep 0.1 - 0.9
+  #sleep 0.$[ ( $RANDOM % 9 ) + 1 ]s # sleep 0.1 - 0.9
 done
