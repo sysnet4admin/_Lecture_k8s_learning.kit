763273f9bf88f8fe7edaeb8c79fa44146579d4e2
Hoon Jo <pagaia@hotmail.com>
2022-06-03 15:32:29 +0900

    fixed code issue

--- A/A.022/6.curl-get-random-sleep.sh
+++ A/A.022/6.curl-get-random-sleep.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-COUNTER = 0
+COUNTER=0
 while true
 do
   COUNTER=$((COUNTER + 1))
-  echo -ne "$COUNTER - " ; curl $1 
+  echo -ne "$COUNTER - " ; curl -s $1 | grep title
   sleep 0.$[ ( $RANDOM % 9 ) + 1 ]s # sleep 0.1 - 0.9
 done
