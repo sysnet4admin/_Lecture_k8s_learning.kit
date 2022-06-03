e594cd2ce0e6ce6c2a50d1141cd36bcf0d271ade
Hoon Jo <pagaia@hotmail.com>
2022-06-03 13:56:06 +0900

    upt cmd

--- A/A.022/.cmd
+++ A/A.022/.cmd
@@ -1,2 +1,7 @@
-kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/" | jq .
-kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/namespaces/default/pods/*/nginx_http_requests" | jq .
+kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/" | jq
+
+kubectl get --raw "/apis/custom.metrics.k8s.io/v1beta2/namespaces/default/pods/*/nginx_http_requests_per_second" | jq
+
+
+
+
