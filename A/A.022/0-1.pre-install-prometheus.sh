562abfe95bc4cce8eb9ef2f1ec1f9561b6ba973b
sysnet4admin <pagaia@hotmail.com>
2022-06-05 19:37:10 +0900

    upt helm pkg

--- A/A.022/0-1.pre-install-prometheus.sh
+++ A/A.022/0-1.pre-install-prometheus.sh
@@ -8,9 +8,6 @@ helm repo update
 helm install prometheus edu/prometheus \
 --set pushgateway.enabled=false \
 --set alertmanager.enabled=false \
---set nodeExporter.tolerations[0].key="node-role.kubernetes.io/master" \
---set nodeExporter.tolerations[0].effect="NoSchedule" \
---set nodeExporter.tolerations[0].operator="Exists" \
 --set server.persistentVolume.storageClass="managed-nfs-storage" \
 --set server.service.type="LoadBalancer" \
 --namespace=monitoring \
