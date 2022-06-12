d2c988b4c5d7e9cadb429cb08c83655f152e97b1
sysnet4admin <pagaia@hotmail.com>
2022-06-12 19:21:29 +0900

    upt ver 2.6.6

--- A/A.021/2.telepresence-2.6.6-installer.sh
+++ A/A.021/2.telepresence-2.6.6-installer.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+VER="2.6.6"
+
 # install pre-requirement  for centos 
 if [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"CentOS"* ]]; then 
   yum install python3 sshfs -y
@@ -7,7 +9,7 @@ if [[ "$(awk -F '=' '/PRETTY_NAME/ { print $2 }' /etc/os-release)" = *"CentOS"* 
 fi 
 
 # download binary and chmod to run 
-curl -fL https://app.getambassador.io/download/tel2/linux/amd64/2.6.2/telepresence -o /usr/local/bin/telepresence
+curl -fL https://app.getambassador.io/download/tel2/linux/amd64/$VER/telepresence -o /usr/local/bin/telepresence
 chmod a+x /usr/local/bin/telepresence
 
 # install traffic-manager 
@@ -16,4 +18,4 @@ helm repo update
 helm install traffic-manager datawire/telepresence \
 --create-namespace \
 --namespace ambassador \
---set image.tag=2.6.2  
+--set image.tag=$VER
