7e85db706a0a8368798b73856146df4f3c1d3c9e
Hoon Jo <pagaia@hotmail.com>
2022-06-15 16:18:35 +0900

    fixed typo

--- A/A.023/.cmd
+++ A/A.023/.cmd
@@ -1,6 +1,11 @@
 cd _Lecture_k8s_learning.kit/A/A.023
 
+pluto
+
 pluto detect-files
+../A.021/3.golang-n-sources-installer.sh
+pluto detect-files
+
 pluto detect-files -o wide 
 pluto detect-files -o markdown 
 pluto detect-files -o csv 
--- A/A.023/pluto-v5.8.0-installer.sh
+++ A/A.023/pluto-v5.8.0-installer.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-VER="5.8.0"
-
 # download binary and move to bin 
-curl -LO https://github.com/FairwindsOps/pluto/releases/download/v$VER/pluto_$VER_linux_amd64.tar.gz
+curl -LO https://github.com/FairwindsOps/pluto/releases/download/v5.8.0/pluto_5.8.0_linux_amd64.tar.gz
 tar xvfz pluto_5.8.0_linux_amd64.tar.gz -C /tmp
 mv /tmp/pluto /usr/local/bin
 
