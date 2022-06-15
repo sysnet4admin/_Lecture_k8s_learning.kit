5bcfe955fbada5e62b3c063c67298a255d0be5d6
Hoon Jo <pagaia@hotmail.com>
2022-06-15 15:53:47 +0900

    upt pluto

---
+++ A/A.023/.cmd
@@ -0,0 +1,10 @@
+cd _Lecture_k8s_learning.kit/A/A.023
+
+pluto detect-files
+pluto detect-files -o wide 
+pluto detect-files -o markdown 
+pluto detect-files -o csv 
+
+pluto detect-files -d ~/_Lecture_k8s_learning.kit/
+pluto detect-files -d ~/_Lecture_k8s_learning.kit/ -o markdown
+
