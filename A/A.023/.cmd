230b28e7366642aaf908644b0efa56a2e3d37d12
Hoon Jo <pagaia@hotmail.com>
2022-06-17 19:45:37 +0900

    upt .cmd

--- A/A.023/.cmd
+++ A/A.023/.cmd
@@ -1,7 +1,9 @@
 cd _Lecture_k8s_learning.kit/A/A.023
 
 # DEMO pluto
-pluto
+./pluto-v5.8.0-installer.sh
+pluto 
+pluto list-versions 
 
 pluto detect-files
 pluto detect-files -o wide 
@@ -11,13 +13,31 @@ pluto detect-files -o csv
 pluto detect-files -d ~/_Lecture_k8s_learning.kit/
 pluto detect-files -d ~/_Lecture_k8s_learning.kit/ -o markdown
 
+pluto detect-files --target-versions k8s=v1.18.0 -o markdown 
+pluto detect-files --target-versions k8s=v1.22.1 -o markdown 
+
 ./pluto-detect-helm-sample/bad-metallb-psp-v1beta1.sh
 pluto detect-helm -o markdown
 
-# DEMO kubectl convert 
 
+# DEMO kubectl convert 
+./kubectl-convert-installer.sh
 kubectl convert --help
 pluto detect-files -o markdown
 
-kubectl convert -f 
+# good
+cat kubectl-convert-sample/good-deploy-svc-nginx-v1beta2.yaml
+kubectl apply -f  kubectl-convert-sample/good-deploy-svc-nginx-v1beta2.yaml
+kubectl delete -f  kubectl-convert-sample/good-deploy-svc-nginx-v1beta2.yaml
+
+kubectl convert -f kubectl-convert-sample/good-deploy-svc-nginx-v1beta2.yaml
+kubectl convert -f kubectl-convert-sample/good-deploy-svc-nginx-v1beta2.yaml | kubectl apply -f -
+
+# bad1
+kubectl convert -f kubectl-convert-sample/bad-ingress-v1beat1.yaml
+cat kubectl-convert-sample/bad-ingress-v1beat1.yaml
+
+# bad2
+kubectl convert -f kubectl-convert-sample/bad-hpa-custom-metrics-v2beta1.yaml
+kubectl convert -f kubectl-convert-sample/bad-hpa-custom-metrics-v2beta1.yaml --output-version autoscaling/v2
 
