c09623614b1d16e903f310adfce2618650d4310b
Hoon Jo <pagaia@hotmail.com>
2022-08-06 20:39:51 +0900

    chg repo addr

--- A/A.022/0-1.pre-install-prometheus.sh
+++ A/A.022/0-1.pre-install-prometheus.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-# helm add edu repo 
-helm repo add edu https://prometheus-community.github.io/helm-charts
+# helm add prometheus-community repo 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
 # actual install prometheus 
-helm install prometheus edu/prometheus \
+helm install prometheus prometheus-community/prometheus \
 --set pushgateway.enabled=false \
 --set alertmanager.enabled=false \
 --set server.persistentVolume.storageClass="managed-nfs-storage" \
