201ac7eddb1c33c987f243441ace64e386474356
Hoon Jo <pagaia@hotmail.com>
2023-01-06 15:43:30 +0900

    add infinite loop

--- A/A.011/01.istio/loadgenerator-infinite.sh
+++ A/A.011/01.istio/loadgenerator-infinite.sh
@@ -3,7 +3,7 @@
 export INGRESS_HOST=$(kubectl -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 
 # infinite loop per 10 second 
-echo "Pres CTRL+C to stop..." 
+echo "Press CTRL+C to stop..." 
 for (( ; ; )) 
 do 
   curl -s -o /dev/null "http://$INGRESS_HOST/productpage"
