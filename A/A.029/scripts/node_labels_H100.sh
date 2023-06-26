a28a3a900ca6ba2c1095e3bc81996eceb4918e0d
Hoon Jo <pagaia@hotmail.com>
2023-06-26 20:06:58 +0900

    add node_labels

---
+++ A/A.029/scripts/node_labels_H100.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#!/usr/bin/env bash
+
+if [ ! -z $1 ] 
+then 
+  for (( c=1; c<=$1; c++ ))
+    do 
+      kubectl label node w$c-k8s gpupool=Nvidia accelerator=Tesla-H100
+    done 
+    : # $1 was given
+else
+    echo "Input labeling node number"
+    : # $1 was not given
+fi
+
