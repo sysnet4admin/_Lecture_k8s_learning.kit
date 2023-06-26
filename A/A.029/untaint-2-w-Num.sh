87d5e43dad933246d556183a8c90dd522b1a28a4
Hoon Jo <pagaia@hotmail.com>
2023-06-26 11:10:56 +0900

    upt taint shell

--- A/A.029/untaint-2-w-Num.sh
+++ A/A.029/untaint-2-w-Num.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
-N = 3 
+if [ ! -z $1 ] 
+then 
+  for (( c=1; c<=$1; c++ ))
+    do 
+      kubectl taint node w$c-k8s kwok-controller/provider=test:NoSchedule-
+    done 
+    : # $1 was given
+else
+    echo "Input untainting node number"
+    : # $1 was not given
+fi
 
-for i in {1..N}
-do 
-  kubectl taint node w{$i}-k8s kwok-controller/provider=test:NoSchedule-
-  echo "untainted on w{$i}-k8s node"
-done 
+
