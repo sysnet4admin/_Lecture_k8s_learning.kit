98273860d1a8080b556f8d52e0ad25593a5f5c67
Hoon Jo <pagaia@hotmail.com>
2023-06-26 19:45:52 +0900

    upt nodename yaml

--- A/A.029/scripts/untaint-2-w-Num.sh
+++ A/A.029/scripts/untaint-2-w-Num.sh
@@ -4,7 +4,7 @@ if [ ! -z $1 ]
 then 
   for (( c=1; c<=$1; c++ ))
     do 
-      kubectl taint node w$c-k8s kwok-controller/provider=test:NoSchedule-
+      kubectl taint node kwok-w$c-k8s kwok-controller/provider=test:NoSchedule-
     done 
     : # $1 was given
 else
