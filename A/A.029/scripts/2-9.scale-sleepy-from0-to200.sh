f6a191f1c54187b9f91b45353362f678898f4125
Hoon Jo <pagaia@hotmail.com>
2023-07-13 15:00:08 +0900

    add exit 0

--- A/A.029/scripts/2-9.scale-sleepy-from0-to200.sh
+++ A/A.029/scripts/2-9.scale-sleepy-from0-to200.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 kubectl scale deploy deploy-sleepy --replicas=0
+sleep 1 
 
   echo -n "Waiting for deploy-sleepy's pod to zero... "
   while true; do 
     if [[ "$(kubectl get po -l app=deploy-sleepy 2>&1)" = *"No resources"* ]]; then
       echo -e "\ndeploy-sleepy have no pod"
       kubectl scale deploy deploy-sleepy --replicas=200
+      exit 0
     else
       # print dot for waiting status 
       echo -n ".";sleep 1
