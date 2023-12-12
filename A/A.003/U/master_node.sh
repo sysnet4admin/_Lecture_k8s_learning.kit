af206e9e9219857a007dde737d96e5bca9d6b05d
Hoon Jo <pagaia@hotmail.com>
2023-12-12 17:31:10 +0900

    add --kubernetes-version for v1.24.x

--- A/A.003/U/master_node.sh
+++ A/A.003/U/master_node.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# init kubernetes (w/ containerd) + image-repo: registry.k8s.io due to v1.24.17 not exit(or gcr.io issue) 
+# init kubernetes (w/ containerd) + # temp: v1.24.x gcr.io not in v1.24.17 only ubuntu issue (kuberentes-version)
 kubeadm init --token 123456.1234567890123456 --token-ttl 0 \
              --pod-network-cidr=172.16.0.0/16 --apiserver-advertise-address=192.168.1.10 \
              --cri-socket=unix:///run/containerd/containerd.sock \
-             --image-repository=registry.k8s.io
+             --kubernetes-version=$1 
 
 # config for master node only 
 mkdir -p $HOME/.kube
