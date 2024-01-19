56eaf3ea971a1daab2da08eae67a2af908393de2
Hoon Jo <pagaia@hotmail.com>
2024-01-19 20:09:12 +0900

    upt .cmd

---
+++ X/X.001/1.configmap-pod-access/.cmd
@@ -0,0 +1,32 @@
+# Create ConfigMaps
+
+k create cm trauerweide --from-literal tree=trauerweide
+
+k apply -f /root/cm.yaml
+
+---
+# Access ConfigMaps in Pod
+
+$cat pod1.yaml 
+apiVersion: v1
+kind: Pod
+metadata:
+  name: pod1
+spec:
+  volumes:
+  - name: birke
+    configMap:
+      name: birke
+  containers:
+  - image: nginx:alpine
+    name: pod1
+    volumeMounts:
+      - name: birke
+        mountPath: /etc/birke
+    env:
+      - name: TREE1
+        valueFrom:
+          configMapKeyRef:
+            name: trauerweide
+            key: tree
+
