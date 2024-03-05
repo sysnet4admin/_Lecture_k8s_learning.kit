0f33c95f1db63ff72a059dc320c318378469bff5
Hoon Jo <pagaia@hotmail.com>
2024-03-05 20:10:32 +0900

    chg kube repo addr

--- B/B.001/U/Before-v1.28/k8s_env_build.sh
+++ B/B.001/U/Before-v1.28/k8s_env_build.sh
@@ -10,11 +10,10 @@ sed -i.bak -r 's/(.+swap.+)/#\1/' /etc/fstab
 
 # add kubernetes repo
 apt-get update && apt-get install apt-transport-https ca-certificates curl
-curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg \
-            https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] \
-      https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+EOF
 
 # add docker-ce repo
 apt-get install -y \
