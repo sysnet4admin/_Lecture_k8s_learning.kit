c6c79aefc85f510dcc8ad2f15d6adf96a3ffd69a
Hoon Jo <pagaia@hotmail.com>
2024-05-20 15:23:23 +0900

    upt v1.30 for ch1/1.15

--- ch1/1.5/k8s-min-5GiB-wo-add-nodes/k_cfg_n_git_clone.sh
+++ ch1/1.5/k8s-min-5GiB-wo-add-nodes/k_cfg_n_git_clone.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
-# install bash-completion for kubectl 
-yum install bash-completion -y 
-
 # kubectl completion on bash-completion dir
 kubectl completion bash >/etc/bash_completion.d/kubectl
 
 # alias kubectl to k 
 echo 'alias k=kubectl' >> ~/.bashrc
+echo "alias ka='kubectl apply -f'" >> ~/.bashrc
 echo 'complete -F __start_kubectl k' >> ~/.bashrc
 
-# git clone k8s-code
+# git clone k8s-adv-code
 git clone https://github.com/sysnet4admin/_Lecture_k8s_learning.kit.git
 mv /home/vagrant/_Lecture_k8s_learning.kit $HOME
 find $HOME/_Lecture_k8s_learning.kit -regex ".*\.\(sh\)" -exec chmod 700 {} \;
+
