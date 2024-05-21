18e520d41c5901aeb0ad4d733127dace49994004
Hoon Jo <pagaia@hotmail.com>
2024-05-21 12:39:29 +0900

    upt v1.30 for ch9/9.5

--- ch9/9.5/metallb-unintaller.sh
+++ ch9/9.5/metallb-unintaller.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-raw_git="raw.githubusercontent.com/sysnet4admin/IaC/master/manifests" 
+RAW_GIT="https://raw.githubusercontent.com/sysnet4admin/IaC/main/k8s/extra-pkgs/v1.30/"
 
-kubectl delete secret memberlist -n metallb-system
-kubectl delete -f https://$raw_git/svc/metallb-l2config.yaml
-kubectl delete -f https://$raw_git/svc/metallb-0.9.6.yaml
+kubectl delete -f "$RAW_GIT/metallb-iprange.yaml"
+kubectl delete -f "$RAW_GIT/metallb-l2mode.yaml"
+kubectl delete -f "$RAW_GIT/metallb-native-v0.14.4.yaml"
 
 echo "MetalLB uninstalled successfully"
 
