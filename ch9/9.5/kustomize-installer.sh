8a574b75d22ae7b3448d976a3650e955d48b044c
Hoon Jo <pagaia@hotmail.com>
2024-05-21 12:52:12 +0900

    upt v1.30 for ch9/9.5

--- ch9/9.5/kustomize-installer.sh
+++ ch9/9.5/kustomize-installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 curl -L \
-        https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.4.1/kustomize_v5.4.1_linux_amd64.tar.gz
+        https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.4.1/kustomize_v5.4.1_linux_amd64.tar.gz \
      -o /tmp/kustomize.tar.gz
 tar -xzf /tmp/kustomize.tar.gz \
     -C  /usr/local/bin
