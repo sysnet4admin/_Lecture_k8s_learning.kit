42ca180cc1e99f28708f006a8af27036d8f837c8
Hoon Jo <pagaia@hotmail.com>
2024-05-21 12:43:08 +0900

    upt v1.30 for ch9/9.5

--- ch9/9.5/metallb-unintaller.sh
+++ ch9/9.5/metallb-unintaller.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-RAW_GIT="https://raw.githubusercontent.com/sysnet4admin/IaC/main/k8s/extra-pkgs/v1.30/"
-
-kubectl delete -f "$RAW_GIT/metallb-iprange.yaml"
-kubectl delete -f "$RAW_GIT/metallb-l2mode.yaml"
-kubectl delete -f "$RAW_GIT/metallb-native-v0.14.4.yaml"
+kubectl delete -f ~/_Lecture_k8s_learning.kit/ch4/4.4/metallb-iprange.yaml
+kubectl delete -f ~/_Lecture_k8s_learning.kit/ch4/4.4/metallb-l2mode.yaml
+kubectl delete -f ~/_Lecture_k8s_learning.kit/ch4/4.4/metallb-native-v0.14.4.yaml
 
 echo "MetalLB uninstalled successfully"
 
