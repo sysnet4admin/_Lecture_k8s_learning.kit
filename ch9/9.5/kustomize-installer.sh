016583fd4d40db65df7e9520da6b68a2fc50b4ab
Hoon Jo <pagaia@hotmail.com>
2024-06-11 20:37:39 +0900

    upt kustomize-installer

--- ch9/9.5/kustomize-installer.sh
+++ ch9/9.5/kustomize-installer.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-curl -L \
-        https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.4.1/kustomize_v5.4.1_linux_amd64.tar.gz \
-     -o /tmp/kustomize.tar.gz
-tar -xzf /tmp/kustomize.tar.gz \
-    -C  /usr/local/bin
+
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/release-kustomize-v5.4.2/hack/install_kustomize.sh"  | bash \
+        > /dev/null
+mv kustomize /usr/local/bin/
 
 echo "kustomize installed successfully"
