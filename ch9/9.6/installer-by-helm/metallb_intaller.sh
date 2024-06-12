0645bb0139930b92f3fe9eb4881d8992d14bf71b
Hoon Jo <pagaia@hotmail.com>
2024-06-12 21:05:51 +0900

    chg comments

--- ch9/9.6/installer-by-helm/metallb_intaller.sh
+++ ch9/9.6/installer-by-helm/metallb_intaller.sh
@@ -8,6 +8,6 @@ helm install metallb edu/metallb \
      --set controller.image.tag=v0.14.5 \
      --set speaker.image.tag=v0.14.5
 
-echo "\nWait 30 seconds for helm CRDs deployed completely" ; sleep 30
+echo "\nWait 30 seconds for helm's CRDs deployed completely" ; sleep 30
 kubectl apply -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crds/
 
