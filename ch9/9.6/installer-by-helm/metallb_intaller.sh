a5ffbb60bcda14eaa57f62beaeb6391f49c9e006
Hoon Jo <pagaia@hotmail.com>
2024-06-12 12:52:42 +0900

    add back slash

--- ch9/9.6/installer-by-helm/metallb_intaller.sh
+++ ch9/9.6/installer-by-helm/metallb_intaller.sh
@@ -7,6 +7,6 @@ helm install metallb edu/metallb \
      --namespace=metallb-system \
      --set controller.image.tag=v0.14.5 \
      --set speaker.image.tag=v0.14.5 \
-     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-l2mode.yaml
+     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-l2mode.yaml \
      -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-iprange.yaml
 
