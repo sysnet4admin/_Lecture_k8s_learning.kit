ae641fc39db0cf0d0df4506c13cf283107107f4d
Hoon Jo <pagaia@hotmail.com>
2024-06-12 12:36:34 +0900

    upt metallb-crd

--- ch9/9.6/installer-by-helm/metallb_intaller.sh
+++ ch9/9.6/installer-by-helm/metallb_intaller.sh
@@ -5,8 +5,8 @@
 helm install metallb edu/metallb \
      --create-namespace \
      --namespace=metallb-system \
-     --set controller.image.tag=v0.10.2 \
-     --set speaker.image.tag=v0.10.2 \
-     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/l2-config-by-helm.yaml
-
+     --set controller.image.tag=v0.14.5 \
+     --set speaker.image.tag=v0.14.5 \
+     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-l2mode.yaml
+     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-iprange.yaml
 
