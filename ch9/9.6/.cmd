378ac73c459e728bab9c716e953bc025894b643b
Hoon Jo <pagaia@hotmail.com>
2024-06-12 10:12:49 +0900

    upt 9.6 all

--- ch9/9.6/.cmd
+++ ch9/9.6/.cmd
@@ -1,20 +1,20 @@
 # helm 
 ~/_Lecture_k8s_learning.kit/ch9/9.6/get_helm.sh
 
-helm repo add edu https://k8s-edu.github.io/helm-charts
+helm repo add edu https://k8s-edu.github.io/Lkv1_main/k8s-adv 
 helm repo update 
 helm search repo
 
 # metallb version 0.14.5 by helm 
 k get po -n metallb-system
-~/_Lecture_k8s_learning.kit/ch9/9.6/uninstaller/metallb-unintaller.sh
+~/_Lecture_k8s_learning.kit/ch9/9.6/uninstaller/metallb_unintaller.sh
 k get po -n metallb-system
 
-cat ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-intaller-by-helm.sh
+cat ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb_intaller.sh
 cat ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-l2mode.yaml 
 cat ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-iprange.yaml 
 
-~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-intaller-by-helm.sh
+~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb_intaller.sh
 helm install metallb edu/metallb \
      --create-namespace \
      --namespace=metallb-system \
@@ -35,13 +35,13 @@ k apply -f ~/_Lecture_k8s_learning.kit/ch9/9.6/deploy-w-pvc-svc.yaml
 k get pv,pvc
 k get storageclasses.storage.k8s.io
 
-cat ~/_Lecture_k8s_learning.kit/ch9/9.6/uninstaller/nfs-provisioner-uninstaller.sh
-~/_Lecture_k8s_learning.kit/ch9/9.6/nfs-provisioner-uninstaller.sh
+cat ~/_Lecture_k8s_learning.kit/ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
+~/_Lecture_k8s_learning.kit/ch9/9.6/nfs-provisioner_uninstaller.sh
 
 k get pv,pvc
 k get storageclasses.storage.k8s.io
 
-~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/nfs-provisioner-installer-by-helm.sh
+~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/nfs-provisioner_installer.sh
 helm install nfs-provisioner edu/nfs-subdir-external-provisioner \
     --set nfs.server=192.168.1.10 \
     --set nfs.path=/nfs_shared/dynamic-vol \
