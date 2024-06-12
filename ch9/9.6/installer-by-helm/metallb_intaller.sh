22a34e920189820821e6af8dd00e11732da5d638
Hoon Jo <pagaia@hotmail.com>
2024-06-12 20:54:01 +0900

    add crds installation

--- ch9/9.6/installer-by-helm/metallb_intaller.sh
+++ ch9/9.6/installer-by-helm/metallb_intaller.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 # Main Source - https://github.com/metallb/metallb
-# path already provided by ch2 from vagrant up
 
 helm install metallb edu/metallb \
      --create-namespace \
      --namespace=metallb-system \
      --set speaker.frr.enabled=false \
      --set controller.image.tag=v0.14.5 \
-     --set speaker.image.tag=v0.14.5 \
-     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-l2mode.yaml \
-     -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/metallb-iprange.yaml
+     --set speaker.image.tag=v0.14.5
+
+echo "\nWait 30 seconds for helm CRDs deployed completely" ; sleep 30
+kubectl apply -f ~/_Lecture_k8s_learning.kit/ch9/9.6/installer-by-helm/metallb-crd/
 
