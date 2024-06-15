63af022cf5fe459aefa82830288abdee91300694
Hoon Jo <pagaia@hotmail.com>
2024-06-15 18:47:14 +0900

    chg uninstaller

--- ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
+++ ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-k apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/nfs-subdir-external-provisioner
-k apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/storageclass.yaml
+kubectl apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/nfs-subdir-external-provisioner
+kubectl apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/storageclass.yaml
 
 echo "NFS-Provisioner uninstalled successfully"
