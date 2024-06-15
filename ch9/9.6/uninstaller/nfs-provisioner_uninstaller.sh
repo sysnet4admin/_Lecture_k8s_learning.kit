397b568fc482a0b60b34fe5fe3a43e395d9b044d
Hoon Jo <pagaia@hotmail.com>
2024-06-15 18:46:10 +0900

    chg uninstaller

--- ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
+++ ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-##### Addtional configuration for All-in-one >> replace to extra-k8s-pkgs
-EXTRA_PKGS_ADDR="https://raw.githubusercontent.com/sysnet4admin/IaC/main/k8s/extra-pkgs/v1.30"
-
-# uninstall nfs-provisioner v4.0.2
-kubectl delete -f $EXTRA_PKGS_ADDR/nfs-provisioner-v4.0.2.yaml
+k apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/nfs-subdir-external-provisioner
+k apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/storageclass.yaml
 
 echo "NFS-Provisioner uninstalled successfully"
