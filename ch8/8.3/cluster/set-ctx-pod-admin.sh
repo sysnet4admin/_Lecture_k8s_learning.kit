c681fcb0866524b8b299d8b7cd5692d910e627ba
Hoon Jo <pagaia@hotmail.com>
2024-08-14 16:09:33 +0900

    create sa token manually

--- ch8/8.3/cluster/set-ctx-pod-admin.sh
+++ ch8/8.3/cluster/set-ctx-pod-admin.sh
@@ -5,6 +5,21 @@ NAMESPACE='default'
 SERVICEACCOUNT='sa-pod-admin'
 SETCRED='set-pod-admin'
 SETCTXNAME='ctx-pod-admin'
+
+# create manually token
+# https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#manually-create-a-long-lived-api-token-for-a-serviceaccount
+TOKENNAME='sa-pod-admin-token'
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $TOKENNAME
+  namespace: $NAMESPACE
+  annotations:
+    kubernetes.io/service-account.name: $SERVICEACCOUNT
+type: kubernetes.io/service-account-token
+EOF
+
 TOKENNAME=`kubectl -n $NAMESPACE get serviceaccount $SERVICEACCOUNT -o jsonpath={.secrets[0].name}`
 
 # confirm token 
