243e7fc64d26753862fd41e067b7efb07898c726
Hoon Jo <pagaia@hotmail.com>
2024-08-14 09:51:54 +0900

    create sa token manually

--- ch8/8.3/dev1/set-ctx-dev1.sh
+++ ch8/8.3/dev1/set-ctx-dev1.sh
@@ -5,7 +5,20 @@ NAMESPACE='dev1'
 SERVICEACCOUNT='dev1-hoon'
 SETCRED='dev1-set-hoon'
 SETCTXNAME='ctx-dev1-hoon'
-TOKENNAME=`kubectl -n $NAMESPACE get serviceaccount $SERVICEACCOUNT -o jsonpath={.secrets[0].name}`
+
+# create manually token 
+# https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#manually-create-a-long-lived-api-token-for-a-serviceaccount
+TOKENNAME='dev1-hoon-token'
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $TOKENNAME
+  namespace: $NAMESPACE
+  annotations:
+    kubernetes.io/service-account.name: $SERVICEACCOUNT
+type: kubernetes.io/service-account-token
+EOF
 
 # confirm token 
 echo "$SERVICEACCOUNT of $NAMESPACE Token: `kubectl -n $NAMESPACE get secret $TOKENNAME -o jsonpath={.data.token}| base64 --decode`"
--- ch8/8.3/dev2/set-ctx-dev2.sh
+++ ch8/8.3/dev2/set-ctx-dev2.sh
@@ -5,7 +5,20 @@ NAMESPACE='dev2'
 SERVICEACCOUNT='dev2-moon'
 SETCRED='dev2-set-moon'
 SETCTXNAME='ctx-dev2-moon'
-TOKENNAME=`kubectl -n $NAMESPACE get serviceaccount $SERVICEACCOUNT -o jsonpath={.secrets[0].name}`
+
+# create manually token
+# https://kubernetes.io/docs/tasks/configure-pod-container/configure-service-account/#manually-create-a-long-lived-api-token-for-a-serviceaccount
+TOKENNAME='dev2-moon-token'
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $TOKENNAME
+  namespace: $NAMESPACE
+  annotations:
+    kubernetes.io/service-account.name: $SERVICEACCOUNT
+type: kubernetes.io/service-account-token
+EOF
 
 # confirm token 
 echo "$SERVICEACCOUNT of $NAMESPACE Token: `kubectl -n $NAMESPACE get secret $TOKENNAME -o jsonpath={.data.token}| base64 --decode`"
