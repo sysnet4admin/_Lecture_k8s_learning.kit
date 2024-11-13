306eafb281ea4b57b2e1f8e73bbb551e53ba6207
Hoon Jo <pagaia@hotmail.com>
2024-11-13 22:00:39 +0900

    rmv previous auto-gen sa

--- ch8/8.3/cluster/set-ctx-pod-admin.sh
+++ ch8/8.3/cluster/set-ctx-pod-admin.sh
@@ -20,8 +20,6 @@ metadata:
 type: kubernetes.io/service-account-token
 EOF
 
-TOKENNAME=`kubectl -n $NAMESPACE get serviceaccount $SERVICEACCOUNT -o jsonpath={.secrets[0].name}`
-
 # confirm token 
 echo "$SERVICEACCOUNT of $NAMESPACE Token: `kubectl -n $NAMESPACE get secret $TOKENNAME -o jsonpath={.data.token}| base64 --decode`"
 
