811edc8dde1623da99d9f51ae30c67b72a421d34
Hoon Jo <pagaia@hotmail.com>
2024-11-16 16:22:52 +0900

    fix typo issue (delete)

--- ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
+++ ch9/9.6/uninstaller/nfs-provisioner_uninstaller.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kubectl apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/nfs-subdir-external-provisioner
-kubectl apply -f ~/_Lecture_k8s_learning.kit/ch5/5.6/storageclass.yaml
+kubectl delete -f ~/_Lecture_k8s_learning.kit/ch5/5.6/nfs-subdir-external-provisioner
+kubectl delete -f ~/_Lecture_k8s_learning.kit/ch5/5.6/storageclass.yaml
 
 echo "NFS-Provisioner uninstalled successfully"
