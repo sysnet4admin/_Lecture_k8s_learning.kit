8e34b00477c6d8cdd0029d5c2aa1e849150ced76
Hoon Jo <pagaia@hotmail.com>
2025-01-25 16:38:52 +0900

    fix script

--- B/B.008/Pod-imagePullPolicy/scripts/cls_unused_img.sh
+++ B/B.008/Pod-imagePullPolicy/scripts/cls_unused_img.sh
@@ -5,6 +5,6 @@ readarray hosts < /etc/hosts
 
 ##clear unused images at all nodes ##
 for host in ${hosts[@]}; do
-  sshpass -p vagrant crictl rmi --prune -f ${host}
+  ssh root@$host crictl rmi --prune
 done
 
