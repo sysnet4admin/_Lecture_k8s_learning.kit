f07a6af78a396aed793ba4f6b1be62544c90077d
Hoon Jo <pagaia@hotmail.com>
2025-01-25 17:08:04 +0900

    fix script

--- B/B.008/Pod-imagePullPolicy/scripts/chk_ps_img.sh
+++ B/B.008/Pod-imagePullPolicy/scripts/chk_ps_img.sh
@@ -8,6 +8,8 @@ ssh root@$HOST 'crictl ps'
 echo -e "\n#2 [ running containers on $HOST ]"
 ssh root@$HOST ls -rlt /var/lib/containerd/io.containerd.grpc.v1.cri/containers/
 
-echo -e "\n#3 [ downloaded images in the last 1 hour on $HOST ]"
-ssh root@$HOST find /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256 -type f -mmin -60
-
+echo -e "\n#3 [ downloaded images in the last 10 mintues on $HOST ]"
+#ssh root@$HOST find /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256 -type f -mmin -60
+# example: 1 hour ago, 2 hours ago, 1 day ago 
+ssh root@$HOST ls -lt /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256/ | \
+               awk -v threshold="$(date -d "10 minutes ago" "+%b %d %H:%M")" '$6" "$7" "$8 >= threshold'
