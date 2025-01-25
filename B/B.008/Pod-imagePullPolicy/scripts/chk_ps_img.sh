c0e06c5195cbe56e1bdf385230d7667bb7814b78
Hoon Jo <pagaia@hotmail.com>
2025-01-25 16:30:37 +0900

    fix script

--- B/B.008/Pod-imagePullPolicy/scripts/chk_ps_img.sh
+++ B/B.008/Pod-imagePullPolicy/scripts/chk_ps_img.sh
@@ -6,7 +6,7 @@ echo -e "#1 [ crictl ps on $HOST ]"
 ssh root@$HOST 'crictl ps'
 
 echo -e "#2 [ running containers on $HOST ]"
-ssh root@$HOST ll /var/lib/containerd/io.containerd.grpc.v1.cri/containers/
+ssh root@$HOST ls -rlt /var/lib/containerd/io.containerd.grpc.v1.cri/containers/
 
 echo -e "#3 [ downloaded images in the last 1 hour on $HOST ]"
 ssh root@$HOST find /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256 -type f -mmin -60
