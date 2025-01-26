c2585e78ed89f43e9efe17ec184502870ec2e44e
Hoon Jo <pagaia@hotmail.com>
2025-01-26 12:16:54 +0900

    upt docker-check

--- B/B.008/Pod-imagePullPolicy/scripts/docker-ratelimit-remaining.sh
+++ B/B.008/Pod-imagePullPolicy/scripts/docker-ratelimit-remaining.sh
@@ -4,6 +4,6 @@
 TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:ratelimitpreview/test:pull" \
         | jq -r .token)
 
-curl --head -H "Authorization: Bearer $TOKEN" \
+curl -s --head -H "Authorization: Bearer $TOKEN" \
                 https://registry-1.docker.io/v2/ratelimitpreview/test/manifests/latest \
-                grep ratelimit
+                | grep ratelimit
