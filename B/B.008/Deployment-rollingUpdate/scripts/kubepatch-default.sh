0888c5b0a02b2b12834038beba589b7bb6f394dc
Hoon Jo <pagaia@hotmail.com>
2025-01-26 15:02:16 +0900

    chg upper to lower

--- B/B.008/Deployment-rollingUpdate/scripts/kubepatch-default.sh
+++ B/B.008/Deployment-rollingUpdate/scripts/kubepatch-default.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-kubectl patch deployment deploy-default-rollingUpdate-maxsurge25 \
-              -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-max25","image":"$1"}]}}}}'
+kubectl patch deployment deploy-default-rollingupdate-maxsurge25 \
+              -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-max25","image":"'$1'"}]}}}}'
 
