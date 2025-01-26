3a4cf602c8f54925527c76e519502acedc21d109
Hoon Jo <pagaia@hotmail.com>
2025-01-26 15:36:04 +0900

    chg kubepatch command

--- B/B.008/Deployment-rollingUpdate/scripts/kubepatch-default.sh
+++ B/B.008/Deployment-rollingUpdate/scripts/kubepatch-default.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 kubectl patch deployment deploy-default-rollingupdate-maxsurge25 \
-              -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-max25","image":"'$1'"}]}}}}'
+              -p '{"spec":{"template":{"spec":{"containers":[{"name":"nginx-max25",\
+                   "image":"quay.io/nginx/nginx-unprivileged:'$1'"}]}}}}'
 
