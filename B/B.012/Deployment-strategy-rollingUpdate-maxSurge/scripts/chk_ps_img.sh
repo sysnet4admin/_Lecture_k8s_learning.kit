b1b5ef4105d1019851a51580f0e2a73a0669a78c
Hoon Jo <pagaia@hotmail.com>
2025-04-01 18:38:38 +0100

    upt B.012

--- B/B.012/Deployment-strategy-rollingUpdate-maxSurge/scripts/chk_ps_img.sh
+++ B/B.012/Deployment-strategy-rollingUpdate-maxSurge/scripts/chk_ps_img.sh
@@ -13,5 +13,4 @@ echo -e "\n#3 [ downloaded images in the last 10 mintues on $HOST ]"
 echo -e "\nlocation: /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256"
 #ssh root@$HOST find /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256 -type f -mmin -60
 # example: 1 hour ago, 2 hours ago, 1 day ago 
-ssh root@$HOST ls -lt /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256/ | \
-               awk -v threshold="$(date -d "10 minutes ago" "+%b %d %H:%M")" '$6" "$7" "$8 >= threshold'
+ssh root@$HOST ls -lt /var/lib/containerd/io.containerd.content.v1.content/blobs/sha256/ | awk -v threshold="$(date -d "10 minutes ago" "+%s")" '{cmd="date -d \""$6" "$7" "$8"\" +%s 2>/dev/null"; cmd | getline timestamp; close(cmd); if(timestamp >= threshold) print $0}'
