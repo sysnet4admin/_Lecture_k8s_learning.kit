bb1dc1ed7a50bbb2c5d0f0d7b4bcf8db7b128571
Hoon Jo <pagaia@hotmail.com>
2026-03-19 08:57:21 +0900

    update Calico CNI manifest path to IaC/k8s/CNI/archived
    Follows IaC repo consolidation (sysnet4admin/IaC@cf8ed4f)

--- A/A.023/pluto-detect-helm-sample/metallb-uninstaller.sh
+++ A/A.023/pluto-detect-helm-sample/metallb-uninstaller.sh
@@ -1,5 +1,5 @@
 # raw_address for gitcontent
-raw_git="raw.githubusercontent.com/sysnet4admin/IaC/master/manifests" 
+raw_git="raw.githubusercontent.com/sysnet4admin/IaC/main/k8s/CNI/archived" 
 
 # config metallb for LoadBalancer service > upgrade
 kubectl delete -f https://$raw_git/svc/metallb-0.10.2.yaml
